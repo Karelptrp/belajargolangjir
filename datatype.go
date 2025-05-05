bea180810104dbbbadc1b4314cfc8a11f94f09f9
Karelptrp <karelpradana@outlook.com>
2025-05-05 15:22:47 +0700

    Add files via upload

---
+++ datatype.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// The main function is the entry point of the program
+// It prints the numbers 1 and 1.2 to the console
+func loop() {
+	fmt.Println(1)
+	fmt.Println(1.2)
+}
