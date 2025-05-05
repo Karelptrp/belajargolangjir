1cfc66a18b34b9ef5ce279a124fab9fc2d56ffa8
Karelptrp <karelpradana@outlook.com>
2025-05-06 05:14:01 +0700

    Update datatype.go

--- datatype.go
+++ datatype.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // The main function is the entry point of the program
 // It prints the numbers 1 and 1.2 to the console
-func loop() {
+func printNumbers() {
 	fmt.Println(1)
 	fmt.Println(1.2)
 }
